67c98c719f633406ffa059cb8eb5ff13b108dc91
Fransisco Wahyu <fransiscowahyu2002@gmail.com>
2025-09-18 22:48:29 +0700

    Update ResumeButton.jsx

--- src/components/ResumeButton.jsx
+++ src/components/ResumeButton.jsx
@@ -1,12 +1,13 @@
-// src/components/ResumeButton.jsx
-import cvUrl from "../assets/Fransisco_Wahyu_resume.pdf";
-
-export default function ResumeButton({ className = "" }) {
+export default function ResumeButton({ className = "", children = "Download CV" }) {
+  const cvUrl = "../assets/Fransisco_Wahyu_resume.pdf"; // file di folder public
   return (
-    <a href={cvUrl} download="Fransisco_Wahyu_CV.pdf"
-       className={`px-4 py-2 rounded-[var(--radius-xl)] bg-brand-500 hover:bg-brand-700 text-white text-sm ${className}`}>
-      Download CV
+    <a
+      href={cvUrl}
+      download="Fransisco Wahyu Syahbani-CV.pdf"
+      className={`inline-flex items-center px-4 py-2 rounded-[var(--radius-xl)] bg-brand-500 hover:bg-brand-700 text-white text-sm ${className}`}
+      title="Download my CV (PDF)"
+    >
+      {children}
     </a>
   );
 }
-
